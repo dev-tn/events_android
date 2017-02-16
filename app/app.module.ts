4552e8b4cfb222373ce218fc5632d7b1fa842e29
teknuk <dev@teknuk.com>
2017-02-16 12:23:52 +0500

    6.6 added new event route  with its two navigation links

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
+import { CreateEventComponent } from './events/create-event.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
@@ -17,6 +18,7 @@ import { appRoutes } from './routes';
     declarations: [
         EventsAppComponent,
         EventsListComponent,
+        CreateEventComponent,
         EventThumbnailComponent,
         EventDetailsComponent,
         NavBarComponent
--- app/routes.ts
+++ app/routes.ts
@@ -3,8 +3,10 @@
 import { Routes} from '@angular/router'
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { CreateEventComponent } from './events/create-event.component'
 
 export const appRoutes:Routes = [
+    { path: 'events/new', component: CreateEventComponent }, //Order of routes matters so we add it on top(path: 'events/new' vs path: 'events/:id')
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent },
     { path: '', redirectTo: '/events', pathMatch: 'full' } //TODO: explore options for pathMatch
