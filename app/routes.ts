6c6205c307f10f11836b3e0846ab80309b6bdf66
teknuk <dev@teknuk.com>
2017-02-16 16:26:35 +0500

    6.12 organizing exports with Barrels

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,14 @@
 //angular provide TS definition for routes config to give extra intellisense and compile time safety
 //so we imported this Routes
 import { Routes} from '@angular/router'
-import { EventsListComponent } from './events/events-list.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event.component'
+import {
+    EventsListComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventListResolver } from './events/events-list-resolver.service'
 
 export const appRoutes:Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent'] }, //Order of routes matters so we add it on top(path: 'events/new' vs path: 'events/:id')
