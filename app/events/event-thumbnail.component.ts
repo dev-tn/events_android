e7821a3b5cb952fdb5f54cd486836c30d5b6165b
teknuk <dev@teknuk.com>
2017-02-16 18:40:36 +0500

    7.2 introduced type saftey for events using model/interface

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core'
+import { IEvent } from './shared/index'
 
 @Component({
     selector: 'event-thumbnail',
@@ -41,7 +42,7 @@ import { Component, Input } from '@angular/core'
     `]
 })
 export class EventThumbnailComponent {
-    @Input() event:any
+    @Input() event:IEvent
 
     // No more using
     //getStartTimeClass() {
