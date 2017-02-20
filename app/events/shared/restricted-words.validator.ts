2ec44c830c3a04274bd99f9d80fe947c9b344c96
teknuk <dev@teknuk.com>
2017-02-20 16:34:05 +0500

    7.12 added a custom validator - restricted words checker

---
+++ app/events/shared/restricted-words.validator.ts
@@ -0,0 +1,16 @@
+import { FormControl } from '@angular/forms'
+
+//TODO: To better understand it watch video [07_12_Creating Custom Validators.mp4]
+export function restrictedWords(words) {
+    return (control:FormControl):{[key: string]: any} => {
+        if (!words) return null
+
+        var invalidWords = words
+            .map(w => control.value.includes(w) ? w : null)
+            .filter(w => w != null)
+
+        return invalidWords && invalidWords.length > 0
+            ? {'restrictedWords': invalidWords.join(', ')}
+            : null
+    }
+}
