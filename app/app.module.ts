e39736c747bffac85ecfb17a73416d9a0ba5c1e1
teknuk <dev@teknuk.com>
2017-02-21 13:03:57 +0500

    10.3 create custom pipe for session duration

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,7 +12,8 @@ import {
     EventDetailsComponent,
     EventThumbnailComponent,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    DurationPipe
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
@@ -39,7 +40,8 @@ import { appRoutes } from './routes';
         Error404Component,
         CreateSessionComponent,
         SessionListComponent,
-        CollapsibleWellComponent
+        CollapsibleWellComponent,
+        DurationPipe
     ],
     providers: [
         EventService,
