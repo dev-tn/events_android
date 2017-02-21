84dd29b231396a7148e55ee60ad3eaa0bb2a03e5
teknuk <dev@teknuk.com>
2017-02-21 12:04:18 +0500

    9.2 added resuable component CollapsibleWell by implementing content projection

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,6 +17,7 @@ import {
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 
@@ -37,7 +38,8 @@ import { appRoutes } from './routes';
         NavBarComponent,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponent
+        SessionListComponent,
+        CollapsibleWellComponent
     ],
     providers: [
         EventService,
