073975a770dab076707b18cf95a8456d364bd2db
teknuk <dev@teknuk.com>
2017-02-22 19:00:17 +0500

    13.7, 13.8 added our custom location validator to NG_VALIDATORS and used it

--- app/events/location-validator.directive.ts
+++ app/events/location-validator.directive.ts
@@ -1,8 +1,10 @@
 import { Directive } from '@angular/core';
-import { Validator, FormGroup } from '@angular/forms';
+import { Validator, FormGroup, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
     selector: '[validateLocation]',
+    // adding our location validator to existing NG_VALIDATORS as indicated by option, multi: true. Otherwise we are overriding NG_VALIDATORS
+    providers: [{provide: NG_VALIDATORS, useExisting: LocationValidator, multi: true}]
 })
 export class LocationValidator implements Validator {
     validate(formGroup:FormGroup):{ [key: string]: any } {
