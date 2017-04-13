c7f7c8393133d295aad3b39a7006afa7e7deaf98
teknuk <dev@teknuk.com>
2017-04-13 12:33:30 +0500

    14.4 used query string params to save and update event data asynchronously by http post request to the server

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core'
-import { Http, Response } from '@angular/http'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
 import { Subject, Observable } from 'rxjs/Rx'
 import {IEvent, ISession} from "./event.model";
 
@@ -22,16 +22,13 @@ export class EventService {
         }).catch(this.handleError);
     }
 
-    saveEvent(event) {
-        event.id = 999
-        event.session = []
-        EVENTS.push(event)
-    }
+    saveEvent(event):Observable<IEvent> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
 
-    updateEvent(event) {
-        let index = EVENTS.findIndex(x => x.id = event.id)
-        console.log("SDWSD")
-        EVENTS[index] = event
+        return this.http.post('/api/events', JSON.stringify(event), options).map((response:Response)=> {
+            return response.json();
+        }).catch(this.handleError);
     }
 
     searchSessions(searchTerm:string) {
