db625f7da323d3f2c5cb70019d598b4c706351fa
teknuk <dev@teknuk.com>
2017-04-13 16:00:41 +0500

    14.8 persisted login status across page refreshes

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { AuthService } from './user/auth.service'
 
 @Component({
     selector: 'events-app',
@@ -10,4 +11,10 @@ import { Component } from '@angular/core'
 
 export class EventsAppComponent {
 
+    constructor(private auth:AuthService) {
+    }
+
+    ngOnInit() {
+        this.auth.checkAuthenticationStatus();
+    }
 }
